1e3d6adece709e33a9e6c56e53ad8a4567159f8b
Shellshok <71099224+mholcey@users.noreply.github.com>
2020-12-30 13:40:51 -0500

    final tweaks to create ReST API
    added class object with def get and return jsonify, added resource for api.

--- flaskapp.py
+++ flaskapp.py
@@ -1,8 +1,29 @@
-from flask import flask
+from flask import Flask
+from flask_restful import Resource, Api
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/') # 'http://www.google.com'
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return "Hello, world!"
+    if(request.method == 'GET'):
 
-app.run(port=5000)
+        data = "Fibonacci Sequence"
+        return jsonify({'data': data})
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n <= 0:
+        print('Incorrect input')
+    elif n == 1:
+        return b
+    else:
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+        return b
+print(fibonacci(21))
+
+if __name__ == '__main__':
+    app.run(debug = True)
